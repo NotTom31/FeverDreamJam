24f79f1fecd91f1db98c334115f51fc7f03d8798
Tom Mezzie <99389013+NotTom31@users.noreply.github.com>
2026-04-20 19:59:54 -0700

    init

---
+++ Source/FeverDream/FeverDream.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FeverDream.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FeverDream, "FeverDream" );
+
+DEFINE_LOG_CATEGORY(LogFeverDream)
---
+++ Source/FeverDream/FeverDreamGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FeverDreamGameMode.h"
+
+AFeverDreamGameMode::AFeverDreamGameMode()
+{
+	// stub
+}
