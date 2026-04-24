d5bf8232f5f9fff903a9c5a7289cad521aa0da60
Thomas Mezzie <thomasmezzie@gmail.com>
2026-04-23 20:10:08 -0700

    Remove starter content

--- Source/FeverDream/FeverDream.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "FeverDream.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FeverDream, "FeverDream" );
-
-DEFINE_LOG_CATEGORY(LogFeverDream)
