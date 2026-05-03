05e780c2c2e36d2a474864ba2bca6d6f039f7f91
Jsbolaris <jsbolaris@gmail.com>
2026-05-02 21:54:43 -0700

    Enhanced Input YAYYYY!!!!!

--- Source/FeverDreamJam/MovementPawn.cpp
+++ Source/FeverDreamJam/MovementPawn.cpp
@@ -3,6 +3,16 @@
 
 #include "MovementPawn.h"
 #include "Components/StaticMeshComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+#include "InputActionValue.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "DrawDebugHelpers.h"
+
+
+
 // Sets default values
 AMovementPawn::AMovementPawn()
 {
@@ -24,38 +34,80 @@ void AMovementPawn::BeginPlay()
 {
 	Super::BeginPlay();
 
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, TEXT("Pawn BeginPlay"));
+	}
+
+	APlayerController* PC = Cast<APlayerController>(GetController());
+	if (!PC)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("No PlayerController"));
+		return;
+	}
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem =
+		ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer());
+
+	if (Subsystem && InputMappingContext)
+	{
+		Subsystem->AddMappingContext(InputMappingContext, 0);
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("Added IMC"));
+	}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Missing IMC or Subsystem"));
+	}
 }
 
 void AMovementPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 	//GetClampedToMaxSize is used to prevent faster diagonal movement when both forward and right input are given
 	if (!MoveInput.IsNearlyZero()) {
 		FVector Movement = MoveInput.GetClampedToMaxSize(1.0f) * MoveSpeed * DeltaTime;
 		AddActorWorldOffset(Movement, true);
 	}
-
+	CheckGrounded();
 }
 
 // Called to bind functionality to input
 void AMovementPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-	PlayerInputComponent->BindAxis("MoveForward", this, &AMovementPawn::MoveForward);
-	PlayerInputComponent->BindAxis("MoveRight", this, &AMovementPawn::MoveRight);
+	UEnhancedInputComponent* EIC = Cast<UEnhancedInputComponent>(InputComponent);
+	if (EIC) {
+		EIC->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AMovementPawn::Move);
+		EIC->BindAction(MoveAction, ETriggerEvent::Completed, this, &AMovementPawn::Move);
+		EIC->BindAction(JumpAction, ETriggerEvent::Started, this, &AMovementPawn::Jump);
+		EIC->BindAction(JumpAction, ETriggerEvent::Completed, this, &AMovementPawn::StopJumping);
+	}
 
 }
 
-void AMovementPawn::MoveForward(float Value)
+void AMovementPawn::Move(const FInputActionValue& Value)
 {
-	MoveInput.X = Value;
+	FVector2D MovementVector = Value.Get<FVector2D>();
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(
+			1,
+			0.f,
+			FColor::Green,
+			Value.ToString()
+		);
+	}
+
+	MoveInput.X = MovementVector.Y; // W/S
+	MoveInput.Y = MovementVector.X; // A/D
 }
 
-void AMovementPawn::MoveRight(float Value)
+void AMovementPawn::Look(const FInputActionValue& Value)
 {
-	MoveInput.Y = Value;
+	FVector2D LookVector = Value.Get<FVector2D>();
+
+	AddControllerYawInput(LookVector.X);
+	AddControllerPitchInput(LookVector.Y);
 }
 
 void AMovementPawn::CheckGrounded()
@@ -69,12 +121,33 @@ void AMovementPawn::CheckGrounded()
 	Params.AddIgnoredActor(this);
 	// this line performs the line trace and sets isGrounded to true if it hits something, false otherwise
 	isGrounded = GetWorld()->LineTraceSingleByChannel(Hit, Start, End, ECC_Visibility, Params);
+
+	DrawDebugLine(
+		GetWorld(),
+		Start,
+		End,
+		isGrounded ? FColor::Green : FColor::Red,
+		false,
+		0.0f,
+		0,
+		2.0f
+	);
+
 }
 
 void AMovementPawn::Jump()
 {
 	if (isGrounded) {
 		//Add an impulse upwards to the mesh component, this will cause the pawn to jump
+		if (GEngine) {
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Grounded, Should be Jumping!")));
+		}
 		Mesh->AddImpulse(FVector(0, 0, 500.0f), NAME_None, true);
 	}
+
+}
+
+void AMovementPawn::StopJumping()
+{ 
+	//Currently does nothing, but could be used to implement variable jump height by reducing the upward velocity when the jump button is released
 }
--- Source/FeverDreamJam/MovementPawn.h
+++ Source/FeverDreamJam/MovementPawn.h
@@ -1,10 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
-
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
-#include "MovementPawn.generated.h"
+#include "InputActionValue.h"
+
+class USpringArmComponent;
+class UInputMappingContext;
+class UInputAction;
+class UCameraComponent;
+#include "MovementPawn.generated.h"		
+
 
 UCLASS()
 class FEVERDREAMJAM_API AMovementPawn : public APawn
@@ -23,26 +28,15 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-	// Called to bind functionality to input
-private:
-	UPROPERTY(VisibleAnywhere)
-	USceneComponent* Root;
-
-	UPROPERTY(VisibleAnywhere)
-	UStaticMeshComponent* Mesh;
-
-	FVector MoveInput;
-	void MoveForward(float Value);
-	void MoveRight(float Value);
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
 	void CheckGrounded();
 	void Jump();
-
-
+	void StopJumping();
 	bool isGrounded = false;
 	UPROPERTY(EditAnywhere)
 	float MoveSpeed = 600.0f;
-	
+
 	UPROPERTY(EditAnywhere)
 	float Acceleration = 3000.0f;
 
@@ -53,7 +47,31 @@ private:
 	float Gravity = -980.0f;
 
 	UPROPERTY(EditAnywhere)
-	float GroundCheckDistance = 100.0f;
+	float GroundCheckDistance = 150.0f;
 
-	
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* Camera;
+	UPROPERTY(EditAnywhere, Category = "Camera")
+	float MouseSensitivity = 1.0f;
+	USceneComponent* Root;
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* Mesh;
+	UPROPERTY(VisibleAnywhere)
+	FVector MoveInput;
+	// Called to bind functionality to input
+
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputMappingContext* InputMappingContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* JumpAction;
+
 };
